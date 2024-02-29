df78b099f7cbe5c50499fe167f49e8adb623976b
Szymon Kalkowski <szymon.kalkowski@wp.pl>
2024-02-29 15:34:12 +0100

    feat: connected with import

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ https://d1hz1p47evrtj0.cloudfront.net/
 ### S3 Bucket URL (private)
 
 https://awscoursebucket07.s3.amazonaws.com/index.html
+
+### Backend repository
+
+https://github.com/szymon-kalkowski/epam-aws-course-be
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://f33d0ydw5m.execute-api.us-east-1.amazonaws.com',
     bff: 'https://dhd3vmaafd.execute-api.us-east-1.amazonaws.com',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
