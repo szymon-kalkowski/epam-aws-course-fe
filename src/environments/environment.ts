9299e210e1b6dd01f412128e6131a2b9ea4cdd6a
Szymon Kalkowski <szymon.kalkowski@wp.pl>
2024-03-01 19:21:17 +0100

    feat: new api gateway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://f33d0ydw5m.execute-api.us-east-1.amazonaws.com',
-    bff: 'https://dhd3vmaafd.execute-api.us-east-1.amazonaws.com',
+    bff: 'https://qacyph5lpb.execute-api.us-east-1.amazonaws.com/',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
